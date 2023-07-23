c35ec06dfcded8cdaac8c24e4085f775c07ae857
RafaelLoureiro <rafanetmello@gmail.com>
2023-07-23 08:25:34 -0300

    ajustado card imagem -icones svg para pixel menor que 400px

--- src/components/InformationContainer.jsx
+++ src/components/InformationContainer.jsx
@@ -4,28 +4,28 @@ import '../styles/components/informationcontainer.sass';
 const InformationContainer = () => {
     return (
         <section id='information'>
-
-            <div id="info-card">
-                <AiFillPhone id='ponhe-icon' />
-                <div>
-                    <h3>Telefone</h3>
-                    <p>(54)98414-6393</p>
+            <div id='cards-Container'>
+                <div id="info-card">
+                    <AiFillPhone id='ponhe-icon' />
+                    <div>
+                        <h3>Telefone</h3>
+                        <p>(54) 98414-6393</p>
+                    </div>
                 </div>
-            </div>
-            <div id="info-card">
-                <AiOutlineMail id='email-icon' />
-                <div>
-                    <h3>E-mail</h3>
-                    <p>rafanetmello@gmail.com</p>
+                <div id="info-card">
+                    <AiOutlineMail id='email-icon' />
+                    <div>
+                        <h3>E-mail</h3>
+                        <p>rafanetmello@gmail.com</p>
+                    </div>
                 </div>
-            </div>
-            <div id="info-card">
-                <AiFillEnvironment id='pin-icon' />
-                <div>
-                    <h3>Localiação</h3>
-                    <p>Nonoai / RS</p>
+                <div id="info-card">
+                    <AiFillEnvironment id='pin-icon' />
+                    <div>
+                        <h3>Localiação</h3>
+                        <p>Nonoai - RS</p>
+                    </div>
                 </div>
-
             </div>
         </section>
     )
