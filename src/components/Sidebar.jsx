b2ef270fed6a3e30aca0e78da5f205bd9e537d19
RafaelLoureiro <rafanetmello@gmail.com>
2023-07-24 19:48:17 -0300

    ajustado curriculo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,7 +12,7 @@ const Sidebar = () => {
             <p className='title'>Desenvolvedor</p>
             <SocialNetworks />
             <InformationContainer />
-            <a href='' className='btn'>
+            <a href="https://drive.google.com/file/d/1C5iPJPSrPVieCq_Y38mB2PJC2pZV8wsR/view?usp=sharing" className='btn'>
                 Download Currículo
             </a>
         </aside>
